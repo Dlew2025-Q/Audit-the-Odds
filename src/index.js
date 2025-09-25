288225242696b2def95053f6e632161df3887f8a
Dlew2025-Q <dalewison@gmail.com>
2025-09-25 15:01:00 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-// FIX: Changed import to lowercase 'app' to match the actual filename.
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
