d3bbefe270369e8df10e72f1cf2217616a394693
Dlew2025-Q <dalewison@gmail.com>
2025-09-25 16:09:26 -0600

    Create StrategyControls.jsx

---
+++ src/StrategyControls.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function StrategyControls() {
+    // This component will handle the parlay strategy options
+    return (
+        <div className="hidden mb-6 p-4 rounded-xl bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800">
+             <h3 className="text-lg font-semibold mb-3 text-center">Parlay Strategy</h3>
+             {/* Strategy controls will be migrated here */}
+        </div>
+    );
+}
